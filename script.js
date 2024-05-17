66dffb57be4fb2b434c52d186aa67c01e39054eb
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-17 15:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // your code here
+const inputs = document.querySelectorAll("input");
+const button = document.querySelector("button");
+const url = document.querySelector("#url");
+
+const Input = {
+  get() {
+    return [...inputs].map((input) => input.value);
+  },
+};
+
+const Url = {
+  base: url.innerText,
+  createQueryString(name, year) {
+    let queryString = ``;
+    if (name && year) queryString = `?name=${name}&year=${year}`;
+    else if (name) queryString = `?name=${name}`;
+    else if (year) queryString = `?year=${year}`;
+    return queryString;
+  },
+  getModified(name, year) {
+    return this.base + this.createQueryString(name, year);
+  },
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const [name, graduationYear] = Input.get();
+  const newUrl = Url.getModified(name, graduationYear);
+  url.innerText = newUrl;
+}
+
+button.addEventListener("click", handleSubmit);
